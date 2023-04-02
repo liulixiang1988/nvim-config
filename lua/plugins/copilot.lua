a8b7f46b25494cbe9b885bf36b28ac3e9b3f3588
Lixiang Liu <liulixiang1988@gmail.com>
2023-04-02 14:45:45 +0800

    Add nvim config

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,19 @@
+vim.g.copilot_assume_mapped = true
+vim.g.copilot_no_tab_map = true
+vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+
+vim.g.copilot_filetypes = {
+    ["*"] = false,
+    ["javascript"] = true,
+    ["typescript"] = true,
+    ["lua"] = true,
+    ["rust"] = true,
+    ["c"] = true,
+    ["c#"] = true,
+    ["c++"] = true,
+    ["go"] = true,
+    ["python"] = true,
+}
+return {
+	"github/copilot.vim"
+}
